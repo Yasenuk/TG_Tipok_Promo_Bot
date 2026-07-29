c4566b0e12130449bffea90ca56cb7137446e4b6
Vladyslav Yatseniuk <yasenukv@gmail.com>
2026-07-29 23:50:09 +0300

    feat: Improve dotenv integration and configuration structure in prisma.config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,12 +1,21 @@
-import dotenv from 'dotenv';
+import 'dotenv';
+
+import { existsSync } from 'node:fs';
 import { defineConfig } from 'prisma/config';
 
+if (existsSync('.env')) {
+  process.loadEnvFile('.env');
+}
+
 export default defineConfig({
-	schema: 'prisma/schema.prisma',
-	migrations: {
-		path: 'prisma/migrations',
-	},
+  schema: 'prisma/schema.prisma',
+
+  migrations: {
+    path: 'prisma/migrations',
+    seed: 'tsx prisma/seed.ts',
+  },
+
 	datasource: {
-		url: process.env.DIRECT_DATABASE_URL,
-	},
+		url: process.env.DIRECT_DATABASE_URL ?? process.env.DATABASE_URL ?? '',
+  },
 });
